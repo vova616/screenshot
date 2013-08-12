6ede4bca1199df7e2646dfcf64e783f6d970d223
Vovchik <vova616@gmail.com>
2013-08-12 11:39:15 +0300

    Panic when using with mac until fixed.

--- screenshot_darwin.go
+++ screenshot_darwin.go
@@ -1,49 +1,17 @@
 package screenshot
 
 import (
-	"code.google.com/p/x-go-binding/xgb"
 	"image"
-	"os"
 )
 
 func ScreenRect() (image.Rectangle, error) {
-	c, err := xgb.Dial(os.Getenv("DISPLAY"))
-	if err != nil {
-		return image.Rectangle{}, err
-	}
-	defer c.Close()
-	x := c.DefaultScreen().WidthInPixels
-	y := c.DefaultScreen().HeightInPixels
-
-	return image.Rect(0, 0, int(x), int(y)), nil
+	panic("ScreenRect is not supported on this platform.")
 }
 
 func CaptureScreen() (*image.RGBA, error) {
-	r, e := ScreenRect()
-	if e != nil {
-		return nil, e
-	}
-	return CaptureRect(r)
+	panic("CaptureScreen is not supported on this platform.")
 }
 
 func CaptureRect(rect image.Rectangle) (*image.RGBA, error) {
-	c, err := xgb.Dial(os.Getenv("DISPLAY"))
-	if err != nil {
-		return nil, err
-	}
-	defer c.Close()
-
-	x, y := rect.Dx(), rect.Dy()
-	xImg, err := c.GetImage(xgb.ImageFormatZPixmap, c.DefaultScreen().Root, int16(rect.Min.X), int16(rect.Min.Y), uint16(x), uint16(y), 0xffffffff)
-	if err != nil {
-		return nil, err
-	}
-
-	data := xImg.Data
-	for i := 0; i < len(data); i += 4 {
-		data[i], data[i+2], data[i+3] = data[i+2], data[i], 255
-	}
-
-	img := &image.RGBA{data, 4 * x, image.Rect(0, 0, x, y)}
-	return img, nil
+	panic("CaptureRect is not supported on this platform.")
 }
