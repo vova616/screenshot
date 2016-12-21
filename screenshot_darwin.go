7dc2637352ceef4786ad44059d654f61d73588d3
Alexandru Sfirlogea <alexandru.sfirlogea@gmail.com>
2016-12-21 15:14:18 +0200

    Implemented all functions using native frameworks

--- screenshot_darwin.go
+++ screenshot_darwin.go
@@ -1,17 +1,53 @@
 package screenshot
 
 import (
+	// #cgo LDFLAGS: -framework CoreGraphics
+	// #cgo LDFLAGS: -framework CoreFoundation
+	// #include <CoreGraphics/CoreGraphics.h>
+	// #include <CoreFoundation/CoreFoundation.h>
+	"C"
 	"image"
+	"reflect"
+	"unsafe"
 )
 
 func ScreenRect() (image.Rectangle, error) {
-	panic("ScreenRect is not supported on this platform.")
+	displayID := C.CGMainDisplayID()
+	width := int(C.CGDisplayPixelsWide(displayID))
+	height := int(C.CGDisplayPixelsHigh(displayID))
+	return image.Rect(0, 0, width, height), nil
 }
 
 func CaptureScreen() (*image.RGBA, error) {
-	panic("CaptureScreen is not supported on this platform.")
+	rect, err := ScreenRect()
+	if err != nil {
+		return nil, err
+	}
+	return CaptureRect(rect)
 }
 
 func CaptureRect(rect image.Rectangle) (*image.RGBA, error) {
-	panic("CaptureRect is not supported on this platform.")
+	displayID := C.CGMainDisplayID()
+	width := int(C.CGDisplayPixelsWide(displayID))
+	rawData := C.CGDataProviderCopyData(C.CGImageGetDataProvider(C.CGDisplayCreateImage(displayID)))
+
+	length := int(C.CFDataGetLength(rawData))
+	ptr := unsafe.Pointer(C.CFDataGetBytePtr(rawData))
+
+	var slice []byte
+	hdrp := (*reflect.SliceHeader)(unsafe.Pointer(&slice))
+	hdrp.Data = uintptr(ptr)
+	hdrp.Len = length
+	hdrp.Cap = length
+
+	imageBytes := make([]byte, length)
+
+	for i := 0; i < length; i += 4 {
+		imageBytes[i], imageBytes[i+2], imageBytes[i+1], imageBytes[i+3] = slice[i+2], slice[i], slice[i+1], slice[i+3]
+	}
+
+	C.CFRelease(rawData)
+
+	img := &image.RGBA{Pix: imageBytes, Stride: 4 * width, Rect: rect}
+	return img, nil
 }
